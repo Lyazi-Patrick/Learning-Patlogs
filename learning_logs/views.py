88d15eefb7b5c631026958a0ceb650ea721a7408
LYAZI PATRICK <Lyazipatrick17@gmail.com>
2025-06-11 17:26:19 +0300

    Added the HTTP404 error message

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
+from django.http import Http404
 # Create your views here.
 def index(request):
     """The home Page for Learning Log"""
@@ -10,7 +11,7 @@ def index(request):
 @login_required
 def topics(request):
     """Show all Topics."""
-    topics = Topic.objects.order_by('date_added')
+    topics = Topic.objects.filter(owner = request.user).order_by('date_added')
     context = {'topics':topics}
     return render(request,'learning_logs/topics.html',context)
 
@@ -18,6 +19,9 @@ def topics(request):
 def topic(request, topic_id):
     """Show a single topic and all its entries."""
     topic = Topic.objects.get(id=topic_id)
+    #Make sure the topic belongs to the curent user . 
+    if topic.owner != request.user:
+        raise Http404
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic':topic, 'entries':entries}
     return render(request, 'learning_logs/topic.html', context)
@@ -67,6 +71,8 @@ def edit_entry(request, entry_id):
     """Edit an Existing Entry."""
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
+    if topic.owner != request.user:
+        raise Http404
 
     if request.method != 'POST':
         #Initial request' pre-fill form with the current entry.
